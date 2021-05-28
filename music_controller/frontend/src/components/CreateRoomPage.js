764ffa374bf809a53ddaf4af2b185a6e2d20c48e
Andre0x1 <andrel.silva3357@gmail.com>
2021-05-28 00:19:50 -0300

    Meio_Video_06 11:20

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,12 +1,59 @@
-import React, {Component} from "react";
+import React, {Component} from "react"
+import {
+    Button,
+    Grid,
+    Typography,
+    TextField,
+    FormHelperText,
+    FormControl,
+    Radio,
+    RadioGroup,
+    FormControlLabel
+} from "@material-ui/core"
+import {link} from "react-router-dom"
 
 
 export default class CreateRoomPage extends Component {
-    constructor(props){
-    super(props);
+    defaultVotes = 2;
+
+    constructor(props) {
+        super(props);
     }
 
-    render(){
-    return <p>Create Room Page</p>
+    render() {
+        return <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography component="h4" variant="h4">
+                    Create A Room
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl component={"fieldset"}>
+                    <FormHelperText>
+                        <div align={'center'}>
+                            Control Playback
+                        </div>
+                    </FormHelperText>
+                    <RadioGroup row defaultValue="true">
+                        <FormControlLabel
+                            value="true"
+                            control={<Radio color="primary"/>}
+                            label="Play/Pause"
+                            labelPlacement={"Bottom"}
+                        >
+
+                        </FormControlLabel>
+                        <FormControlLabel
+                            value="false"
+                            control={<Radio color="Secondary"/>}
+                            label="No Control"
+                            labelPlacement={"Bottom"}
+                        >
+
+                        </FormControlLabel>
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+        </Grid>
     }
 }
